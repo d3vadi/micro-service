75d90359b5de17aa473171edf0933d2585bb44c7
d3vadi <100505688+d3vadi@users.noreply.github.com>
2024-01-31 12:36:19 +0530

    Project setup

---
+++ micro-service/src/main/kotlin/com/deva/microservice/MicroServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.deva.microservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MicroServiceApplication>(*args)
+}
